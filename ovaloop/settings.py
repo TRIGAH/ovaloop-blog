3770bfd272dd679078aeff6a9a0901025652723a
Trigah <ijenrandy2@gmail.com>
2024-07-28 14:53:36 +0100

    images fix

--- ovaloop/settings.py
+++ ovaloop/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = "ovaloop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-	"default": dj_database_url.parse("postgresql://maps:vlNLibUEbvToGmgbnXsKciMU90IhiDCT@dpg-cqif848gph6c738oca70-a.oregon-postgres.render.com/ovaloopdevserver")
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+# DATABASES = {
+# 	"default": dj_database_url.parse("postgresql://maps:vlNLibUEbvToGmgbnXsKciMU90IhiDCT@dpg-cqif848gph6c738oca70-a.oregon-postgres.render.com/ovaloopdevserver")
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -128,11 +128,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_URL = '/static/'
+# # STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [(BASE_DIR / "static"),]
+# STATICFILES_DIRS = [(BASE_DIR / "static"),]
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles/static_root'
