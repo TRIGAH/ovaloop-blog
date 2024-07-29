38ddee1bfdc3b2dbd2bf16d6fd9ec38399efd4a6
Trigah <ijenrandy2@gmail.com>
2024-07-29 04:46:19 +0100

    remove staticfiles

--- blog/urls.py
+++ blog/urls.py
@@ -15,9 +15,3 @@ urlpatterns = [
 
 ]
 
-
-    # path('', views.post_list, name='post_list'),
-    # path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    # path('post/new/', views.post_new, name='post_new'),
-    # path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    # path('notifications/', views.notification_list, name='notification_list'),
